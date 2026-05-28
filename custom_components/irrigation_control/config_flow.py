7d8cf9ae90c54f0a99293c659c2b2d298d03cab7
Richard Scott <richard.scott@zenntrix.com>
2026-05-28 14:13:15 +0100

    Update config_flow.py

--- custom_components/irrigation_control/config_flow.py
+++ custom_components/irrigation_control/config_flow.py
@@ -12,25 +12,33 @@ def configured_instances(hass):
 
 class IrrigationControlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title=user_input["name"], data=user_input)
+	    if user_input is not None:
+	        return self.async_create_entry(title=user_input["name"], data=user_input)
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("name", description="Name of the irrigation zone"): str,
-                vol.Required("irrigation_zone_switch_entity_id", description="Select the switch entity for the irrigation zone"): selector({
-                    "entity": {"domain": "switch"}
-                }),
-                vol.Required("irrigation_pump_switch_entity_id", description="Select the switch which controls the irrigation pump"): selector({
-                    "entity": {"domain": "light"}
-                }),
-                vol.Required("water_consumption_entity_id", description="Select the sensor entity for water consumption"): selector({
-                    "entity": {"domain": "sensor"}
-                }),
-                vol.Required("volume_required", description="Volume of water required to turn off the irrigation (in liters)"): vol.Coerce(float),
-            })
-        )
+	    return self.async_show_form(
+	        step_id="user",
+	        data_schema=vol.Schema({
+	            vol.Required("name"): str,
+	            vol.Required("irrigation_zone_switch_entity_id"): selector({
+	                "entity": {"domain": "switch"}
+	            }),
+	            vol.Required("irrigation_pump_switch_entity_id"): selector({
+	                "entity": {"domain": "light"}
+	            }),
+	            vol.Required("water_consumption_entity_id"): selector({
+	                "entity": {"domain": "sensor"}
+	            }),
+	            vol.Required("volume_required"): vol.Coerce(float),
+	        }),
+	        # Pass descriptions as `extra` UI hints
+	        description_placeholders={
+	            "name": "Name of the irrigation zone",
+	            "irrigation_zone_switch_entity_id": "Select the switch entity for the irrigation zone",
+	            "irrigation_pump_switch_entity_id": "Select the light entity which controls the 	irrigation pump",	
+            "water_consumption_entity_id": "Select the sensor entity for water consumption",
+          	  "volume_required": "Volume of water required to turn off the irrigation (in liters)",
+        	}
+    	)
 
     @staticmethod
     @callback
